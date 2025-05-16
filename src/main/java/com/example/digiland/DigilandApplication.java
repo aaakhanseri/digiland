411596c659b4f3c3f0ab001f26e2343ebdf30c22
Ali <114877417+Akhanseri@users.noreply.github.com>
2025-05-16 04:20:10 +0200

    Digiland

---
+++ src/main/java/com/example/digiland/DigilandApplication.java
@@ -0,0 +1,13 @@
+package com.example.digiland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigilandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigilandApplication.class, args);
+    }
+
+}
